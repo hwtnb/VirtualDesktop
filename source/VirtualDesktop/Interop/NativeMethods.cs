a929042f8b8a382720d65a2234adb721cf9953fb
hwtnb <hiroki.w@gmail.com>
2022-01-26 01:18:28 +0900

    Fix unexpected exception after Windows Explorer restarts. * This error occurs when application is running as administrator.

--- source/VirtualDesktop/Interop/NativeMethods.cs
+++ source/VirtualDesktop/Interop/NativeMethods.cs
@@ -14,7 +14,33 @@ namespace WindowsDesktop.Interop
 		[DllImport("user32.dll", CharSet = CharSet.Unicode)]
 		public static extern uint RegisterWindowMessage(string lpProcName);
 
+		[DllImport("user32.dll", SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		public static extern bool ChangeWindowMessageFilterEx(IntPtr hwnd, uint message, uint action, ref CHANGEFILTERSTRUCT pChangeFilterStruct);
+
 		[DllImport("user32.dll")]
 		public static extern bool CloseWindow(IntPtr hWnd);
+	}
+
+	public enum MessageFilter : uint
+	{
+		MSGFLT_RESET = 0,
+		MSGFLT_ALLOW = 1,
+		MSGFLT_DISALLOW = 2
+	};
+
+	public enum MessageFilterInfo : uint
+	{
+		MSGFLTINFO_NONE = 0,
+		MSGFLTINFO_ALREADYALLOWED_FORWND = 1,
+		MSGFLTINFO_ALREADYDISALLOWED_FORWND = 2,
+		MSGFLTINFO_ALLOWED_HIGHER = 3,
+	};
+
+	[StructLayout(LayoutKind.Sequential)]
+	public struct CHANGEFILTERSTRUCT
+	{
+		public uint cbSize;
+		public MessageFilterInfo ExtStatus;
 	}
 }
