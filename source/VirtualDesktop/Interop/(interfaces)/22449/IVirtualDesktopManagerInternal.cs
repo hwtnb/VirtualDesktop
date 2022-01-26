2df8f30fa073c923827e769ed5a67a8d45664f8f
hwtnb <hiroki.w@gmail.com>
2022-01-26 14:01:09 +0900

    Fix error on Windows 11 version 21H2 (original release build 22000).

--- source/VirtualDesktop/Interop/(interfaces)/22449/IVirtualDesktopManagerInternal.cs
+++ source/VirtualDesktop/Interop/(interfaces)/22449/IVirtualDesktopManagerInternal.cs
@@ -17,6 +17,8 @@ namespace WindowsDesktop.Interop
 
 		IVirtualDesktop GetCurrentDesktop(IntPtr hWndOrMon);
 
+		IObjectArray GetAllCurrentDesktops();//7
+
 		IObjectArray GetDesktops(IntPtr hWndOrMon);
 
 		IVirtualDesktop GetAdjacentDesktop(IVirtualDesktop pDesktopReference, AdjacentDesktop uDirection);
@@ -31,18 +33,18 @@ namespace WindowsDesktop.Interop
 
 		IVirtualDesktop FindDesktop([In, MarshalAs(UnmanagedType.LPStruct)] Guid desktopId);
 
-		void GetDesktopSwitchIncludeExcludeViews(IVirtualDesktop desktop, out IObjectArray includeViews, out IObjectArray excludeViews);//14
+		void GetDesktopSwitchIncludeExcludeViews(IVirtualDesktop desktop, out IObjectArray includeViews, out IObjectArray excludeViews);//15
 
 		void SetDesktopName(IVirtualDesktop pDesktop, [MarshalAs(UnmanagedType.HString)] string chName);
 
 		void SetDesktopWallpaper(IVirtualDesktop pDesktop, [MarshalAs(UnmanagedType.HString)] string chPath);
 
-		void UpdateWallpaperPathForAllDesktops([MarshalAs(UnmanagedType.HString)] string wallpaper);//17
+		void UpdateWallpaperPathForAllDesktops([MarshalAs(UnmanagedType.HString)] string wallpaper);//18
 
-		void CopyDesktopState(IApplicationView pView0, IApplicationView pView1);//18
+		void CopyDesktopState(IApplicationView pView0, IApplicationView pView1);//19
 
-		int GetDesktopIsPerMonitor();//19
+		int GetDesktopIsPerMonitor();//20
 
-		void SetDesktopIsPerMonitor(int state);//20
+		void SetDesktopIsPerMonitor(int state);//21
 	}
 }
---
+++ source/VirtualDesktop/Interop/(wrappers)/VirtualDesktopManagerInternal22449.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace WindowsDesktop.Interop
+{
+	[ComInterfaceWrapper("IVirtualDesktopManagerInternal", 22449)]
+	internal sealed class VirtualDesktopManagerInternal22449 : VirtualDesktopManagerInternal21313
+	{
+		public VirtualDesktopManagerInternal22449(ComInterfaceAssembly assembly)
+			: base(assembly)
+		{
+		}
+	}
+}
